4842d77db99ec8321b866c4a70ed21f671e807cc
amin mardani <amin.mardanisoror.sf@gmail.com>
2025-03-13 03:39:15 +0330

    psuh product

--- src/assets/js/manage.js
+++ src/assets/js/manage.js
@@ -1,16 +1,72 @@
-// window.onload = function() {
-// let data =  localStorage.getItem('newProduct')
-  
-// productList.innerHTML = data
+// ==========
+// push product card
+// ==========
+// window.onload = function () {
+//   let data = JSON.parse(localStorage.getItem("newItems"));
+//   if(data == null) {
+//     productList.innerHTML = 'محصولی ساخته نشده'
+//   }else{
+
+//     function renderProducts(products) {
+//       const template = products.map((product) => {
+//         return `
+
+//   <div class="border borderClr-primary pb-5 flex flex-col justify-center bg-white">
+//   <div class="flex justify-between w-11/12 mx-auto text-xl">
+//    <button class="text-red-600 deleteProduct" > حذف </button>
+//    <button> ویرایش </button>
+
+//   </div>
+//                     <img src="${product.img}" alt="" />
+//                     <div class="flex flex-col gap-3 mt-3 text-center">
+//                       <span>${product.name}</span>
+//                       <span class="primary-color">${product.rate}</span>
+//                       <span>${product.price}</span>
+
+//                     </div>
+//                      <a
+//                       href=""
+//                       class="border w-max mx-auto py-1 borderClr-primary px-4"
+//                       >خرید</a>
+//                   </div>
+
+//   `;
+//       }).join("");
+//       productList.innerHTML = template;
+//     }
+
+//     renderProducts(data);
+//   }
+//  // ==========
+// // end of push product card
+// // ===========
+//   // ==========
+// // delete product card
+// // ===========
+
+// let deleteProduct = document.querySelectorAll('.deleteProduct')
+
+// function removeCart() {
+//   console.log('amin mardani');
+
 // }
 
+// deleteProduct.addEventListener('click',removeCart())
+
+// // ==========
+// // end of delete product card
+// // ===========
+// };
 
 
+// ==========
+// make product card
+// ===========
 
 const productForm = document.getElementById("productForm");
 const productList = document.getElementById("productList");
 
-productForm.addEventListener("submit", function (event) {
+function makeProduct(event) {
   event.preventDefault();
 
   const productName = document.getElementById("productName").value;
@@ -21,7 +77,7 @@ productForm.addEventListener("submit", function (event) {
   const reader = new FileReader();
   reader.onloadend = function () {
     const productCard = document.createElement("div");
-    productCard.className = " ";
+    
     productCard.innerHTML = `  
             <div class="border borderClr-primary pb-5 flex flex-col justify-center bg-white">
                   <img src="${reader.result}" alt="${productName}" />
@@ -36,27 +92,74 @@ productForm.addEventListener("submit", function (event) {
                     >خرید</a>
                 </div>
         `;
-const itemsData = {
-  name: productName,
-  price : productPrice,
-  rate :productRating,
-  img : productImage,
-}
-itemsDataStorage=[]
-itemsDataStorage.push(itemsData)
-console.log(itemsDataStorage);
+    const itemsData = {
+      name: productName,
+      price: productPrice,
+      rate: productRating,
+      img: reader.result,
+    };
+    let itemsDataStorage = [];
+    if (localStorage.getItem("newItems") == null) {
+      itemsDataStorage.push(itemsData);
+      localStorage.setItem("newItems", JSON.stringify(itemsDataStorage));
+      console.log(itemsDataStorage);
+    } else {
+      itemsDataStorage = JSON.parse(localStorage.getItem("newItems"));
+      itemsDataStorage.push(itemsData);
+
+      localStorage.setItem("newItems", JSON.stringify(itemsDataStorage));
+    }
 
     productList.appendChild(productCard);
-    
+
     productForm.reset();
- 
-    
   };
 
   if (productImage) {
     reader.readAsDataURL(productImage);
   }
   
-  
-  
-});
+  localStorage.setItem("madeProduct", "yes");
+}
+
+productForm.addEventListener("submit", makeProduct);
+// ==========
+//end of make product card
+// ===========
+
+
+// ==========
+//push product form storage
+// ===========
+let data = JSON.parse(localStorage.getItem("newItems"));
+
+
+if (data != null) {
+
+
+  function renderProducts(products) {
+    const template = products.map((product) => {
+      return `
+         <div class="border borderClr-primary pb-5 flex flex-col justify-center bg-white">
+                    <img src="${product.img}"  />
+                    <div class="flex flex-col gap-3 mt-3 text-center">
+                      <span>${product.name}</span>
+                      <span class="primary-color">${product.rate}</span>
+                      <span>${product.price}</span> 
+                    </div>
+                     <a
+                      href=""
+                      class="border w-max mx-auto py-1 borderClr-primary px-4"
+                      >خرید</a>
+                  </div>
+        `;
+    }).join('');
+    productList.innerHTML = template
+  }
+  renderProducts(data)
+}
+// ==========
+//end of push product form storage
+// ==========
+
+
